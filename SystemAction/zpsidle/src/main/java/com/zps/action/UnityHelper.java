5a301ede2e6ad2b3c71bfc9c0209c1cb906ffa3d
zizhen.huang <53505495@qq.com>
2019-05-22 17:13:44 +0800

    Issue:修改bug

--- SystemAction/zpsidle/src/main/java/com/zps/action/UnityHelper.java
+++ SystemAction/zpsidle/src/main/java/com/zps/action/UnityHelper.java
@@ -1,25 +1,37 @@
 package com.zps.action;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 import com.zstart.action.AppActionProxy;
+import com.zstart.action.util.LogUtil;
 import com.zstart.action.util.SystemUtil;
 
 public class UnityHelper {
-    private static AppActionProxy actionProxy;
-    private static Context rootContext;
-    public static void init(Context context){
+    private AppActionProxy actionProxy;
+    private Context rootContext;
+    public void init(Context context){
+        LogUtil.d("init unity helper!!!");
         rootContext = context;
         actionProxy = new AppActionProxy();
         actionProxy.setContext(context);
     }
 
-    public static void installApp(String path,String pkg){
+    public void installApp(String path,String pkg){
         actionProxy.install(path, pkg,"com.zps.idle");
     }
 
-    public static void runApp(String pkg){
-        actionProxy.run(pkg);
+    public void runApp(String pkg){
+        LogUtil.d("try run app that pkg = " + pkg);
+        try{
+            PackageManager packageManager = rootContext.getPackageManager();
+            Intent intent = packageManager.getLaunchIntentForPackage(pkg);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            rootContext.getApplicationContext().startActivity(intent);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void shutdown(){
